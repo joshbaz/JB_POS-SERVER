34ddda32194b7736ff0abd5e52d5c49224534355
Kimbareeba <jkimbareeba@yahoo.com>
2026-04-25 20:10:02 +0300

    cors changes

--- src/index.js
+++ src/index.js
@@ -17,7 +17,12 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
